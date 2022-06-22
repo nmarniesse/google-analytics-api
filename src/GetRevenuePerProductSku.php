2328e5bedcec1039c2f15a3a30576d6ccc47aa87
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2022-06-22 17:58:48 +0200

    Get revenue per sku

---
+++ src/GetRevenuePerProductSku.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Akeneo PIM Enterprise Edition.
+ *
+ * (c) 2022 Akeneo SAS (https://www.akeneo.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nmarniesse\GoogleAnalyticsApi;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class GetRevenuePerProductSku extends Command
+{
+    protected static $defaultName = 'ga:revenue:fetch';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('Test.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $analytics = $this->initializeAnalytics();
+        $profileId = $this->getFirstProfileId($analytics);
+
+        $gaData = $analytics->data_ga->get(
+            'ga:' . $profileId,
+            '7daysAgo',
+            'today',
+            'ga:itemRevenue,ga:uniquePurchases',
+            [
+                'dimensions' => ["ga:productSku"],
+            ]
+        );
+
+        print_r("Rows:\n");
+        print_r($gaData->getRows());
+
+        return Command::SUCCESS;
+    }
+
+    function initializeAnalytics()
+    {
+        // Creates and returns the Analytics Reporting service object.
+
+        // Use the developers console and download your service account
+        // credentials in JSON format. Place them in this directory or
+        // change the key file location if necessary.
+        $KEY_FILE_LOCATION = __DIR__ . '/../service-account-credentials.json';
+
+        // Create and configure a new client object.
+        $client = new \Google_Client();
+        $client->setApplicationName("Hello Analytics Reporting");
+        $client->setAuthConfig($KEY_FILE_LOCATION);
+        $client->setScopes(['https://www.googleapis.com/auth/analytics.readonly']);
+        $analytics = new \Google_Service_Analytics($client);
+
+        return $analytics;
+    }
+
+    function getFirstProfileId($analytics)
+    {
+        // Get the user's first view (profile) ID.
+
+        // Get the list of accounts for the authorized user.
+        $accounts = $analytics->management_accounts->listManagementAccounts();
+
+        if (count($accounts->getItems()) > 0) {
+            $items = $accounts->getItems();
+            $firstAccountId = $items[0]->getId();
+
+            // Get the list of properties for the authorized user.
+            $properties = $analytics->management_webproperties
+                ->listManagementWebproperties($firstAccountId);
+
+            if (count($properties->getItems()) > 0) {
+                $items = $properties->getItems();
+                $firstPropertyId = $items[0]->getId();
+
+                // Get the list of views (profiles) for the authorized user.
+                $profiles = $analytics->management_profiles
+                    ->listManagementProfiles($firstAccountId, $firstPropertyId);
+
+                if (count($profiles->getItems()) > 0) {
+                    $items = $profiles->getItems();
+
+                    // Return the first view (profile) ID.
+                    return $items[0]->getId();
+
+                } else {
+                    throw new \Exception('No views (profiles) found for this user.');
+                }
+            } else {
+                throw new \Exception('No properties found for this user.');
+            }
+        } else {
+            throw new \Exception('No accounts found for this user.');
+        }
+    }
+}
